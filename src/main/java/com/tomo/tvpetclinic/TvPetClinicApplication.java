91248c728c10cde2d5392a5e802d9a68ea946434
Tomo <tomo.vucic@gmail.com>
2022-01-05 22:01:08 +0100

    Initial Project Add

---
+++ src/main/java/com/tomo/tvpetclinic/TvPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.tomo.tvpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomo/tvpetclinic/TvPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomo.tvpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
