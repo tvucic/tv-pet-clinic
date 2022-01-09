565a61d0061d655d007e960d51a35edb322e4849
Tomo <tomo.vucic@gmail.com>
2022-01-09 17:36:12 +0100

    refactoring into multi module project Closes #1 issue.

--- pet-clinic-data/src/main/java/com/tomo/tvpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/tomo/tvpetclinic/model/Owner.java
@@ -1,7 +1,5 @@
 package com.tomo.tvpetclinic.model;
 
-import java.time.LocalDate;
-
 public class Owner extends Person
 {
 }
---
+++ pet-clinic-web/src/test/java/com/tomo/tvpetclinic/TvPetClinicApplicationTest.java
@@ -0,0 +1,8 @@
+package com.tomo.tvpetclinic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TvPetClinicApplicationTest
+{
+
+}
