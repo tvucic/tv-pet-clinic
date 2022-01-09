a01b9152491bc69b0c3b79011b46c42b7bdadd4d
Tomo <tomo.vucic@gmail.com>
2022-01-09 20:16:17 +0100

    Add BaseEntity class. Closes #10 issue.

--- pet-clinic-data/src/main/java/com/tomo/tvpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/tomo/tvpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.tomo.tvpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet
+public class Pet extends BaseEntity
 {
     private LocalDate birthDate;
     private PetType petType;
