d9145ec4b79924806818fbfba963e471936c96d3
Tomo <tomo.vucic@gmail.com>
2022-01-22 15:29:28 +0100

    added speciality set to Vet class. closes #28 issue.

---
+++ pet-clinic-data/src/main/java/com/tomo/tvpetclinic/model/Speciality.java
@@ -0,0 +1,16 @@
+package com.tomo.tvpetclinic.model;
+
+public class Speciality extends BaseEntity
+{
+    private String description;
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+}
