22e75d0e22a41c3a0a7fff449a5a6519f962c976
Tomo <tomo.vucic@gmail.com>
2022-01-22 15:25:04 +0100

    added visits for vets, updated Owner with Pet relationship closes #27 issue.

--- pet-clinic-data/src/main/java/com/tomo/tvpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/tomo/tvpetclinic/model/Owner.java
@@ -1,5 +1,18 @@
 package com.tomo.tvpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person
 {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets()
+    {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets)
+    {
+        this.pets = pets;
+    }
 }
