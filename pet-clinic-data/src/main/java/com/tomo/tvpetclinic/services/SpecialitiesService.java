50844ce2abda7824c82009861842155c4278bcee
Tomo <tomo.vucic@gmail.com>
2022-01-25 20:37:24 +0100

    Created Specialty Map Service Impl closes #32 issue.

---
+++ pet-clinic-data/src/main/java/com/tomo/tvpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.tomo.tvpetclinic.services;
+
+import com.tomo.tvpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends  CrudService<Speciality, Long>
+{
+}
--- pet-clinic-data/src/main/java/com/tomo/tvpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/tomo/tvpetclinic/services/VetService.java
@@ -2,7 +2,6 @@ package com.tomo.tvpetclinic.services;
 
 import com.tomo.tvpetclinic.model.Vet;
 
-
 public interface VetService extends CrudService<Vet, Long>
 {
 }
